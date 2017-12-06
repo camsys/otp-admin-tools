463a2918367bca29eb0a451243a4ed20dc371600
Derek Edwards <dedwards8@gmail.com>
2017-12-06 10:03:24 -0500

    Add ATIS/OTP time helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,28 @@ module ApplicationHelper
   def yield_content(content_key)
     view_flow.content.delete(content_key)
   end
+
+  def otp_time_str otp_time
+    #OTP Time comes in as Epoch integer
+    Time.at(otp_time.to_i/1000).in_time_zone.strftime("%I:%M %p")
+  end
+
+  def atis_time_str atis_time
+    #ATIS time comes in as a string in the format HHMM" 
+    ampm = "AM"
+    hours = atis_time[0..1]
+    mins = atis_time[2..3]
+
+    if hours.to_i == 00
+      hours = "12"
+    elsif hours.to_i > 12
+      hours = (hours.to_i - 12).to_s
+      ampm = "PM"
+    elsif hours.to_i == 12
+      ampm = "PM"
+    end
+      
+    "#{hours}:#{mins} #{ampm}"
+
+  end
 end
