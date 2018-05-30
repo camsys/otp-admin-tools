8bd9575fe1a9da02afece3e1b8971f271274c64a
Derek Edwards <dedwards8@gmail.com>
2018-05-30 14:28:05 -0400

    Handle bikeTime and driveTime in OTP response.

--- app/models/concerns/comparison_tools.rb
+++ app/models/concerns/comparison_tools.rb
@@ -20,6 +20,8 @@ module ComparisonTools
                     start_time: itin["startTime"], 
                     end_time: itin["endTime"], 
                     walk_time: itin["walkTime"], 
+                    drive_time: itin["driveTime"],
+                    bike_time: itin["bikeTime"],
                     transit_time: itin["transitTime"], 
                     waiting_time: itin["waitingTime"],
                     walk_distance: itin["walkDistance"],
