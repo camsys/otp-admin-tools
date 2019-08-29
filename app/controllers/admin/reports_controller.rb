a770ffd716e8d7aec0e0a9b64b39703504b11217
Tazeen Mahtab <tmahtab@camsys.com>
2019-08-29 15:32:43 -0700

    Chart export

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Admin::ReportsController < Admin::AdminController
   
   DASHBOARDS = ['API Usage', 'Origin Destination']
@@ -181,6 +183,30 @@ class Admin::ReportsController < Admin::AdminController
   
   ### / graphical dashboards  
   
+  ### CSV TABLE DOWNLOADS ###
+
+  def download_table
+
+    redirect_to({
+      controller: 'reports', 
+      action: 'chart_table', 
+      format: :csv,
+      params: params
+    })
+  end
+  
+  def chart_table
+
+    rows = params[:data]
+    csv_str = rows.inject([]) { |csv, row|  csv << CSV.generate_line(row) }.join("")
+
+    respond_to do |format|
+      format.csv { send_data csv_str }
+    end
+  end
+
+  ### / csv table downloads
+
   protected
 
   def add_commas(num_string)
