893511f8b81173c1d79ed0f749bbad401da01afb
tmahtab <tmahtab@camsys.com>
2019-11-12 09:23:47 -0500

    Fix shoryuken config for Figaro loading issue

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -3,6 +3,11 @@ Aws.config.update({
   credentials: Aws::Credentials.new(ENV['SQS_AWS_ACCESS_KEY'], ENV['SQS_AWS_SECRET_KEY'])
 })
 
+Shoryuken.configure_server do |config|
+  Shoryuken.add_group('default', 1)
+  Shoryuken.add_queue(ENV['SQS_QUEUE'], 1, 'default')
+end
+
 Shoryuken.configure_client do |config|
   config.sqs_client = Aws::SQS::Client.new(
     region: ENV['AWS_SQS_REGION'],
